493c44c72712a2b0030663e812992e1e32870734
Hanna Kozak <hannawkozak@gmail.com>
2022-05-13 11:46:16 +0100

    style: Home page basic style

--- client/src/views/Home/Home.styled.tsx
+++ client/src/views/Home/Home.styled.tsx
@@ -3,21 +3,61 @@ import styled from 'styled-components';
 export const HomeWrapper = styled.div`
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  font-size: 2rem;
   text-align: center;
-  border-radius: 15px;
+
+  @media ${({ theme }) => theme.device.tablet} {
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 2rem;
+    text-align: center;
+    border-radius: 15px;
+  }
 `;
+
 export const HomeImage = styled.img`
-  width: 40%;
+  width: 100%;
+  @media ${({ theme }) => theme.device.mobileM} {
+    width: 70%;
+  }
+  @media ${({ theme }) => theme.device.tablet} {
+    width: 50%;
+  }
+  @media ${({ theme }) => theme.device.laptop} {
+    width: 40%;
+  }
 `;
 
 export const HomeText = styled.div`
-  padding: 1rem;
+  margin: 2rem;
+  letter-spacing: 1px;
+  line-height: 1.6;
+
   & h1,
   h2 {
     color: ${({ theme }) => theme.colors.primary3};
+    font-size: 2rem;
+  }
+
+  @media ${({ theme }) => theme.device.tablet} {
+    & h1,
+    h2 {
+      color: ${({ theme }) => theme.colors.primary3};
+      font-size: 2.5rem;
+    }
+  }
+  @media ${({ theme }) => theme.device.tablet} {
+    & h1,
+    h2 {
+      color: ${({ theme }) => theme.colors.primary3};
+      font-size: 3rem;
+    }
   }
 `;
 
--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import homeImage from '../../assets/images/undraw_Events_re_98ue.png';
-import { HomeImage, HomeWrapper, HomeText, HomeTitle } from './Home.styled';
+import { HomeImage, HomeWrapper, HomeText } from './Home.styled';
 export const Home = () => {
   return (
     <HomeWrapper>
       <HomeImage src={homeImage} alt="undraw_Events" />
       <HomeText>
         <h1>Manage Your plans easily </h1>
-        <p> with</p>
+        <h2> with</h2>
         <h2>Event Planner App </h2>
         <button>Log in</button>
         <p> Do not have an account yet? Please sign up</p>
