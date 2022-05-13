1ae463e050028cb8e0db069d76e2f40fd6af4208
Hanna Kozak <hannawkozak@gmail.com>
2022-05-13 11:45:47 +0100

    feat: add breakpoint to theme

--- client/src/styles/themes/theme.ts
+++ client/src/styles/themes/theme.ts
@@ -1,13 +1,16 @@
 import { colors, Colors } from './theme/colors';
 import { text, Text } from './theme/text';
+import { device, DeviceSize } from './theme/device'
 
 export type ThemeType = {
     colors: Colors;
-    text: Text
+    text: Text,
+    device: DeviceSize
 };
 
 
 export const theme: ThemeType = {
     colors,
-    text
+    text,
+    device
 };
