691642b585de13bc404b00121aa72ddef251d977
Hanna Kozak <hannawkozak@gmail.com>
2022-05-23 21:11:20 +0100

    feat: add custom forminput component

---
+++ client/src/components/FormInput/FormInput.tsx
@@ -0,0 +1,27 @@
+import React, { HTMLInputTypeAttribute } from 'react';
+import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
+import { InputStyled, LabelStyled } from './FormInput.styled';
+
+export type FormInputProps = {
+  error?: undefined | FieldError;
+  register: UseFormRegisterReturn;
+  name: string;
+  type: HTMLInputTypeAttribute;
+  label: string;
+};
+
+export const FormInput = ({
+  error,
+  register,
+  name,
+  label,
+  type,
+}: FormInputProps) => {
+  return (
+    <>
+      <LabelStyled htmlFor={name}>{label}</LabelStyled>
+      <InputStyled id={name} type={type} {...register} />
+      {error && <span>{error.message}</span>}
+    </>
+  );
+};
