df72e6a8b894e2ff42acc0a13c189da806678c7d
Hanna Kozak <hannawkozak@gmail.com>
2022-05-27 00:00:30 +0100

    feat: add login route to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './views/Home/Home';
 import { Signup } from './views/Signup/Signup';
+import { Login } from './views/Login/Login';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
