76dc128c4fa4bd898ede6eea941246f0dc92d900
Hanna Kozak <hannawkozak@gmail.com>
2022-06-03 21:54:33 +0100

    feat: add authUser dashboard view

--- client/src/views/Dashboard/Dashboard.tsx
+++ client/src/views/Dashboard/Dashboard.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import { useAuth } from '../../context/authContext';
+import React, { useEffect, useState } from 'react';
+import { useFetch } from '../../hooks/useFetch';
 
 export const Dashboard = () => {
-  const { isAuthorized, authUser } = useAuth();
+  const [authUser, setAuthUser] = useState<AuthUserType>();
+  const { sendRequest } = useFetch();
 
-  return (
-    <>{isAuthorized() && <p>Welcome {(authUser as any).loginUser.name}</p>}</>
-  );
+  type AuthUserType = {
+    _id: number;
+    name: string;
+    email: string;
+  };
+
+  const getUser = async () => {
+    try {
+      const responseData = await sendRequest(
+        '/api/users/authUser',
+        'GET',
+        null,
+        {
+          credentials: 'include',
+        },
+      );
+      setAuthUser((responseData as any).data);
+    } catch (err) {
+      throw new Error('Problem with fetching data');
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, [sendRequest]);
+
+  return <>{authUser && <p>Welcome {authUser.name} </p>}</>;
 };
