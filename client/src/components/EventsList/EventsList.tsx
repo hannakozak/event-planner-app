35bed11861e3c2e250ea366dfe94863db603f645
Hanna Kozak <hannawkozak@gmail.com>
2022-06-13 22:39:00 +0100

    feat: add event list component

---
+++ client/src/components/EventsList/EventsList.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import { EventsWrapper } from './EventsList.styled';
+
+type EventType = {
+  title: string;
+  description: string;
+  date: string;
+  time: string;
+};
+
+export const EventsList = () => {
+  const [eventsList, setEventsList] = useState<EventType[]>();
+  const { sendRequest } = useFetch();
+
+  const getEvents = async () => {
+    try {
+      const responseData = await sendRequest(
+        '/api/events/userEvents',
+        'GET',
+        null,
+        {
+          credentials: 'include',
+        },
+      );
+      setEventsList((responseData as any).data);
+      console.log(responseData);
+    } catch (err) {
+      throw new Error('Problem with fetching events list');
+    }
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, [sendRequest]);
+
+  return (
+    <EventsWrapper>
+      <h1>Daily Events</h1>
+      {eventsList && eventsList.map((event) => event.title)}
+    </EventsWrapper>
+  );
+};
