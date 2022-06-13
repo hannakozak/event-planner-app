5a59e5db2d9fbf4a61cf10986e69e2d3160822eb
Hanna Kozak <hannawkozak@gmail.com>
2022-06-13 22:41:15 +0100

    feat: add user logout

--- client/src/context/authContext.tsx
+++ client/src/context/authContext.tsx
@@ -1,7 +1,10 @@
 import React, { createContext, useContext } from 'react';
+import { useFetch } from '../hooks/useFetch';
+import { useNavigate } from 'react-router-dom';
 
 type AuthContextType = {
   isAuthorized: () => boolean;
+  logout: () => Promise<void>;
 };
 
 type Props = {
@@ -11,14 +14,29 @@ type Props = {
 const AuthContext = createContext({} as AuthContextType);
 
 const AuthProvider = ({ children }: Props) => {
+  const { sendRequest } = useFetch();
+  const navigate = useNavigate();
+
   const isAuthorized = () => {
     return document.cookie !== null;
+  };
+
+  const logout = async () => {
+    try {
+      await sendRequest('/api/users/logout', 'GET', null, {
+        credentials: 'include',
+      });
+      navigate('/login');
+    } catch (err) {
+      throw new Error('Problem with logiut user');
+    }
   };
 
   return (
     <AuthContext.Provider
       value={{
         isAuthorized,
+        logout,
       }}
     >
       {children}
